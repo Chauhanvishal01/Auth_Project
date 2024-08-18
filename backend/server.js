ced8be78f8b8a084dde4f17dfcdf978b654a570a
Vishal Chauhan <vishal2825m@gmail.com>
2024-08-18 16:01:14 +0530

    Create SignUp route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 import { connection } from "./database/db.connection.js";
 dotenv.config();
 const app = express();
 
+app.use(express.json());
 
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 connection();
 app.listen(process.env.PORT, () => {
